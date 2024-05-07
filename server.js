15d5d0f21a86b62763ed78e1d815d4c3230da82d
anonymous <133980811+ridwan414-hub@users.noreply.github.com>
2024-05-08 00:43:12 +0600

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const __dirname = path.dirname(__filename);
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, ',/client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 
 app.use('/api/v1/auth', authRoutes);
